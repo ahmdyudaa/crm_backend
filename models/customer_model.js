a6eab56dd063ac2fe7f3816be4c6e00d3c7ee070
berlyand25 <berlyand0007@gmail.com>
2024-05-22 16:47:47 +0700

    tambahan

--- models/customer_model.js
+++ models/customer_model.js
@@ -2,23 +2,23 @@ const db = require('../config/database');
 
 const Customer = {
     getAll: (callback) => {
-        const sql = 'SELECT * FROM customers';
+        const sql = 'SELECT * FROM customer';
         db.query(sql, callback);
     },
     getById: (id, callback) => {
-        const sql = 'SELECT * FROM customers WHERE id = ?';
+        const sql = 'SELECT * FROM customer WHERE id_customer = ?';
         db.query(sql, [id], callback);
     },
     create: (data, callback) => {
-        const sql = 'INSERT INTO customers SET ?';
-        db.query(sql, data, callback);
+        const sql = 'INSERT INTO customer (id_prospek, alamat, posisi, id_alamat) VALUES (?,?,?,?)';
+        db.query(sql, [data.id_prospek, data.alamat, data.posisi, data.id_alamat], callback);
     },
     update: (id, data, callback) => {
-        const sql = 'UPDATE customers SET ? WHERE id = ?';
+        const sql = 'UPDATE customer SET ? WHERE id_customer = ?';
         db.query(sql, [data, id], callback);
     },
     delete: (id, callback) => {
-        const sql = 'DELETE FROM customers WHERE id = ?';
+        const sql = 'DELETE FROM customer WHERE id_customer = ?';
         db.query(sql, [id], callback);
     }
 };
