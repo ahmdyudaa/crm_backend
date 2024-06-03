3365b08e75ce0491c5ae134736e685b490b3886b
berlyand25 <berlyand0007@gmail.com>
2024-06-03 20:54:35 +0700

    fix route getAkunByUsername

--- routes/akun_route.js
+++ routes/akun_route.js
@@ -4,7 +4,7 @@ const akunController = require('../controllers/akun_controller');
 
 router.get('/', akunController.getAllAkun);
 router.get('/:id', akunController.getAkunById);
-router.get('/:username', akunController.getAkunByUsername);
+router.get('/username/:username', akunController.getAkunByUsername);
 router.post('/', akunController.createAkun);
 router.put('/:id', akunController.updateAkun);
 router.delete('/:id', akunController.deleteAkun);
